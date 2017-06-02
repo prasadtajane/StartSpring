9144c73b28701a93b6e6a6524254aea0f319090b
Prasad Tajane <tajane.pr@husky.neu.edu>
2017-06-02 16:32:29 -0500

    First - get Bean with @Componenet

---
+++ SpringOne/src/main/java/com/prasad/learnspring/SpringOne/Application.java
@@ -0,0 +1,13 @@
+package com.prasad.learnspring.SpringOne;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages={"com.prasad.learnspring.", "org.prasad.learspring"})
+//By Default it scans the package and its sub-packages in which Application (configuration) class is present
+
+public class Application {
+	
+}
